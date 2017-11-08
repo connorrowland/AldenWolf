b0cd655f405ac81bf6c6a8180a6ba8b2eee621e7
Connor Rowland <connor.rowland34@gmail.com>
2017-11-08 15:23:11 -0500

    Animation work

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,21 @@
 window.addEventListener('scroll', navShrink);
 window.addEventListener('scroll', boxFadeIn);
 window.addEventListener('scroll', boxFadeInTwo);
-window.addEventListener('scroll', textOneFadeIn);
 window.addEventListener('scroll', textTwoFadeIn);
 window.addEventListener('scroll', stampFadeIn);
 window.addEventListener('scroll', stampTwoFadeIn);
 window.addEventListener('scroll', stampThreeFadeIn);
 
+var isMobile = $(window).width() <= 768 ? true : false;
+if(isMobile){
+  window.addEventListener('scroll', textColMobileFadeInOne);
+  window.addEventListener('scroll', textColMobileFadeInTwo);
+  window.addEventListener('scroll', mobileTextThree);
+  window.addEventListener('scroll', mobileTextFour);
+}
+if(!isMobile){
+  window.addEventListener('scroll', textOneFadeIn);
+}
 function navShrink(){
   if($(window).scrollTop() >= 30){
     $("#logo").addClass("logo--scroll");
@@ -21,10 +30,10 @@ function boxFadeInTwo(){
   fadeInWhenBottomReached("#ethos-header", "#blue-square", '', boxFadeInTwo, false, "#ethos-body-text")
 }
 function textOneFadeIn(){
-  fadeInWhenBottomReached("#downward-arrow", "#section-3-left-column--text", '#section-3-right-column--text', textOneFadeIn, true, "#blue-square")
+  fadeInWhenBottomReached("#downward-arrow", "#section-3-left-column--text", '#section-3-right-column--text', textOneFadeIn, true, "")
 }
 function textTwoFadeIn(){
-  fadeInWhenBottomReached("#home--section-3", "#section-4-center-column--text", '', textTwoFadeIn, false, "#mobile-body-1")
+  fadeInWhenBottomReached("#home--section-3", "#section-4-center-column--text", '', textTwoFadeIn, false, "#section-3-right-column--text")
 }
 function stampFadeIn(){
   fadeInWhenBottomReached("#section-6--heading", "#small-black-circle", "", stampFadeIn, false, "#section-6--subheading-3")
@@ -36,9 +45,24 @@ function stampThreeFadeIn(){
   fadeInWhenBottomReached("#blue-circle", "#orange-circle", "", stampThreeFadeIn, false, "#blue-circle")
 }
 
+//Exclusively Mobile Animations
+function textColMobileFadeInOne(){
+  fadeInWhenBottomReachedMobile("#section-3-left-column--text", textColMobileFadeInOne, "#blue-square")
+}
+function textColMobileFadeInTwo(){
+  fadeInWhenBottomReachedMobile("#section-3-right-column--text", textColMobileFadeInTwo, "#section-3-left-column--text")
+}
+function mobileTextThree(){
+  fadeInWhenBottomReachedMobile("#section-5-mobile-fade", mobileTextThree, "#section-4-center-column--text")
+}
+function mobileTextFour(){
+  fadeInWhenBottomReachedMobile("#section-6-mobile-fade", mobileTextFour, "#section-5-mobile-fade")
+}
+
+//Animation Function (Mobile and Desktop)
 function fadeInWhenBottomReached(bottomObject, objectOneToAnimate, objectTwoToAnimate, listenerToTerminate, backToBackFade, mobileBottomObj){
-  var isMobile = $(window).width() <= 768 ? true : false;
-  if (isMobile){
+  var smallScreen = $(window).width() <= 768 ? true : false;
+  if (smallScreen){
     var bottom_of_object = $(mobileBottomObj).offset().top + $(mobileBottomObj).outerHeight() + 300;
   } else {
     var bottom_of_object = $(bottomObject).offset().top + $(bottomObject).outerHeight() + 320;
@@ -53,6 +77,16 @@ function fadeInWhenBottomReached(bottomObject, objectOneToAnimate, objectTwoToAn
     } else {
       $(objectOneToAnimate).css('visibility', 'visible').hide().fadeIn()
     }
+    window.removeEventListener('scroll', listenerToTerminate);
+  }
+}
+
+//Exclusively Mobile Animation Function
+function fadeInWhenBottomReachedMobile(mobileObjectToAnimate, listenerToTerminate, mobileBottomObj){
+  var bottom_of_object = $(mobileBottomObj).offset().top + $(mobileBottomObj).outerHeight() + 300;
+  var bottom_of_window = $(window).scrollTop() + $(window).height();
+  if( bottom_of_window > bottom_of_object ){
+    $(mobileObjectToAnimate).css('visibility', 'visible').hide().fadeIn(700)
     window.removeEventListener('scroll', listenerToTerminate);
   }
 }
